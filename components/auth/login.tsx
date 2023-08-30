0f9756de8eaa0598238fded21bf840e87a5e825c
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-08-30 23:00:24 +0200

    login errors and UI updated

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import { Box, Button, Center, FormControl, FormLabel, FormErrorMessage, Input, Text, Heading } from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
 
+// Función para iniciar sesión
 export const loginUser = async (credentials) => {
     try {
         const response = await axios.post('/api/auth/login', credentials);
         if (response.data.message === "Inicio de sesión exitoso!") {
-            return true; // Retornar verdadero si el inicio de sesión es exitoso
+            return true;
         } else {
             console.log(response.data);
             return false;
@@ -17,28 +21,76 @@ export const loginUser = async (credentials) => {
     }
 };
 
+// Esquema de validación
+const validationSchema = Yup.object({
+    email: Yup.string().email('Correo no válido').required('Campo requerido'),
+    password: Yup.string().required('Campo requerido'),
+});
+
 function Login() {
     const router = useRouter();
-    const [credentials, setCredentials] = useState({ "email": '', "password": '' });
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
 
-    const handleChange = (e) => {
-        setCredentials({ ...credentials, [e.target.name]: e.target.value });
-    }
+    const handleSubmit = async (values, actions) => {
+        const loginSuccess = await loginUser(values);
+        actions.setSubmitting(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const loginSuccess = await loginUser(credentials);
         if (loginSuccess) {
+            setSuccessMessage("Inicio de sesión exitoso!");
+            setErrorMessage(null);
             router.push('/dashboard');
+        } else {
+            setErrorMessage("Fallo en el inicio de sesión. Por favor, inténtelo de nuevo.");
+            setSuccessMessage(null);
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="email" name="email" placeholder="Email" onChange={handleChange} />
-            <input type="password" name="password" placeholder="Contraseña" onChange={handleChange} />
-            <button type="submit">Iniciar sesión</button>
-        </form>
+        <>
+            <Box mb={8} textAlign="center" mt={20}>
+                <Heading size="2xl">Iniciar Sesión</Heading>
+            </Box>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+            >
+                {(props) => (
+                    <Form>
+                        <Box m={450} spacing={4} mt={6}>
+                            <Field name='email'>
+                                {({ field, form }) => (
+                                    <FormControl isInvalid={form.errors.email && form.touched.email}>
+                                        <FormLabel fontWeight="bold">Email:</FormLabel>
+                                        <Input {...field} type="email" placeholder="Email" />
+                                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+
+                            <Field name='password'>
+                                {({ field, form }) => (
+                                    <FormControl isInvalid={form.errors.password && form.touched.password}>
+                                        <FormLabel mt={10} fontWeight="bold">Contraseña:</FormLabel>
+                                        <Input {...field} type="password" placeholder="Contraseña" />
+                                        <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+
+                            <Center>
+                                <Button mt={10} colorScheme='teal' type="submit" isLoading={props.isSubmitting}>
+                                    Iniciar sesión
+                                </Button>
+                            </Center>
+                        </Box>
+                        {errorMessage && <Text color="red.500" fontSize="xl" fontWeight="bold" textAlign="center">{errorMessage}</Text>}
+                        {successMessage && <Text color="green.500" fontSize="xl" fontWeight="bold" textAlign="center">{successMessage}</Text>}
+                    </Form>
+                )}
+            </Formik>
+        </>
     );
 }
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,6 @@ import Login from '../components/auth/login';
 function LoginPage() {
     return (
         <div>
-            <h1>Iniciar Sesión</h1>
             <Login />
         </div>
     );

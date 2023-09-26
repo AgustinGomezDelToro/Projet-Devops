d82722a492bb1e5fed1ac9a1b4395f9c778ae913
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-09-26 17:21:40 +0200

    lista de pacientes ordenado alfabeticamente y barra de busqueda integrada

--- components/Pacientes/PatientsList.tsx
+++ components/Pacientes/PatientsList.tsx
@@ -1,24 +1,29 @@
 import { useState, useEffect } from 'react';
-import { Box, Table, Thead, Tbody, Tr, Th, Td, chakra } from "@chakra-ui/react";
+import {  Input, Stack, Box, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+
 
 const PatientsList: React.FC = () => {
     const [patients, setPatients] = useState<any[]>([]);
     const [users, setUsers] = useState<{ [key: number]: string }>({});
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const patientsResponse = await fetch('/api/Patients/Patients');
-                const usersResponse = await fetch('/api/users/users');  // Asegúrate de que esta ruta sea la correcta
+                const usersResponse = await fetch('/api/users/users');
 
                 if (!patientsResponse.ok || !usersResponse.ok) {
                     throw new Error('Error al obtener los datos');
                 }
 
                 const patientsData = await patientsResponse.json();
+                const sortedPatients = patientsData.sort((a, b) => a.name.localeCompare(b.name));
+
                 const usersData = await usersResponse.json();
 
-                setPatients(patientsData);
+                setPatients(sortedPatients);
 
                 const usersMap: { [key: number]: string } = {};
                 usersData.forEach((user: any) => {
@@ -34,9 +39,27 @@ const PatientsList: React.FC = () => {
         fetchData();
     }, []);
 
+    function normalizeString(str: string): string {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    }
+
+    const filteredPatients = patients.filter(patient =>
+        normalizeString(patient.name).includes(normalizeString(searchTerm))
+    );
+
     return (
         <Box overflowX="auto">
-            <Table w="full" size="sm">
+            <Box mb={6} mt={3} ml={2} mr={2}>
+                <Stack spacing={6}>
+                    <Input
+                        size='md'
+                        placeholder="Buscar paciente por nombre..."
+                        focusBorderColor='pink.400'
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </Stack>
+            </Box>
+            <Table w="full" size="sm" variant="striped" colorScheme="gray">
                 <Thead>
                     <Tr>
                         <Th>Nombre</Th>
@@ -47,7 +70,7 @@ const PatientsList: React.FC = () => {
                     </Tr>
                 </Thead>
                 <Tbody>
-                    {patients.map(patient => (
+                    {filteredPatients.map(patient => (
                         <Tr key={patient.id}>
                             <Td>{patient.name}</Td>
                             <Td>{patient.email}</Td>

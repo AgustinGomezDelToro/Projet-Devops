deefd61780005f0d22c86d6766b4bb1aa1cec36a
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-11-17 20:10:28 +0100

    api auth js to type finished

--- pages/api/auth/logout.tsx
+++ pages/api/auth/logout.tsx
@@ -3,10 +3,10 @@ import cookie from 'cookie';
 
 export default async function logoutHandler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== "POST") {
-        return res.status(405).end(); // Método no permitido
+        return res.status(405).end(); // Méthode non autorisée
     }
 
-    // Elimina la cookie al establecer su fecha de vencimiento en el pasado
+    // Supprime le cookie en fixant sa date d'expiration dans le passé
     res.setHeader(
         'Set-Cookie',
         cookie.serialize('myTokenName', '', {
@@ -18,5 +18,5 @@ export default async function logoutHandler(req: NextApiRequest, res: NextApiRes
         })
     );
 
-    return res.json({ message: "Sesión cerrada exitosamente" });
+    return res.json({ message: "Session fermée avec succès" });
 }

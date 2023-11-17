230f000cf888c6c0b138495de021b21bb4505a9c
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-11-17 20:19:22 +0100

    api Profile js to type finished

--- pages/api/profile/profile.tsx
+++ pages/api/profile/profile.tsx
@@ -1,18 +1,21 @@
-import {verify} from 'jsonwebtoken';
+import { verify } from 'jsonwebtoken';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function profileHandler(req, res) {
-    const {myTokenName} = req.cookies;
+export default function profileHandler(req: NextApiRequest, res: NextApiResponse) {
+    const { myTokenName } = req.cookies;
 
     if (!myTokenName) {
-        return res.status(401).json({ message: "No estás autenticado." });
+        return res.status(401).json({ message: "Vous n'êtes pas authentifié." });
     }
 
     try {
-        const user = verify(myTokenName, process.env.JWT_SECRET);
-        console.log("User decoded from token:", user);
+        const user = verify(myTokenName, process.env.JWT_SECRET as string);
+        console.log("Utilisateur décodé du token:", user);
         return res.json(user);
     } catch (error) {
-        console.error("Error verifying token:", error);
-        return res.status(500).json({ message: "Token inválido o ha ocurrido un error al decodificarlo." });
+        if (error instanceof Error) {
+            console.error("Erreur lors de la vérification du token:", error.message);
+            return res.status(500).json({ message: "Token invalide ou erreur lors du décodage.", details: error.message });
+        }
     }
 }

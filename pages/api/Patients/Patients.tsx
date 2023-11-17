a1d6cb18a5ccf406b696d7625279778d5340e122
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-11-17 20:16:54 +0100

    api Patients js to type finished

--- pages/api/Patients/Patients.tsx
+++ pages/api/Patients/Patients.tsx
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default async function patientsHandler(req, res) {
+export default async function patientsHandler(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
 
     switch (method) {
@@ -10,15 +11,16 @@ export default async function patientsHandler(req, res) {
             try {
                 const patients = await prisma.pacientes.findMany(); // Asume que tienes una tabla llamada 'pacientes'
                 res.status(200).json(patients);
-            } catch (e) {
-                res.status(500).json({ error: "No se pudieron obtener los pacientes" });
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.error("Erreur lors de la récupération des patients:", error.message);
+                    res.status(500).json({ error: "Erreur lors de la récupération des patients", details: error.message });
+                }
             }
             break;
 
-        // Aquí puedes agregar más casos si deseas realizar otras operaciones como POST, PUT, DELETE, etc.
-
         default:
-            res.setHeader('Allow', ['GET']); // Si solo vas a permitir GET, solo coloca GET en este array.
-            res.status(405).end(`Method ${method} Not Allowed`);
+            res.setHeader('Allow', ['GET']);
+            res.status(405).json({ error: `Méthode ${method} non autorisée` });
     }
 }

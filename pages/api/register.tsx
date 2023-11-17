4707c069d446e33371f9743ca5af053d26fcf262
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-11-17 20:23:53 +0100

    api register js to type finished

--- pages/api/register.tsx
+++ pages/api/register.tsx
@@ -1,26 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 import { hash } from "bcrypt";
-import {router} from "next/client";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default async function handle(req, res) {
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Méthode non autorisée' });
   }
 
   const { name, email, password, role } = req.body;
 
-// Validar el correo electrónico
+  // Valider le email
   const existingUser = await prisma.user.findUnique({ where: { email } });
   if (existingUser) {
-    return res.status(400).json({ error: 'El email ya existe en nuestro sistema, por favor elige otro.' });
+    return res.status(400).json({ error: 'L\'email existe déjà dans notre système, veuillez en choisir un autre.' });
   }
 
-
-  // Hashear la contraseña
+  // Hashear le mdp
   const hashedPassword = await hash(password, 10);
-
 
   try {
     const newUser = await prisma.user.create({
@@ -36,7 +34,9 @@ export default async function handle(req, res) {
 
     res.status(200).json(newUser);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while processing your request' });
+    if (error instanceof Error) {
+      console.error("Erreur lors du traitement de votre demande:", error.message);
+      res.status(500).json({ error: 'Une erreur est survenue lors du traitement de votre demande', details: error.message });
+    }
   }
 }

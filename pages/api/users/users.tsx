1434d5c77f6624d666d96e70a49e2744e7fae467
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-11-17 20:22:32 +0100

    api users js to type finished

--- pages/api/users/users.tsx
+++ pages/api/users/users.tsx
@@ -9,9 +9,12 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
             const users = await prisma.user.findMany({ select: { id: true, name: true } });
             res.status(200).json(users);
         } catch (error) {
-            res.status(500).json({ error: "Error al obtener los usuarios" });
+            if (error instanceof Error) {
+                console.error("Erreur lors de la récupération des utilisateurs:", error.message);
+                res.status(500).json({ error: "Erreur lors de la récupération des utilisateurs", details: error.message });
+            }
         }
     } else {
-        res.status(405).end(); // Method Not Allowed
+        res.status(405).json({ error: 'Méthode non autorisée' });
     }
 }

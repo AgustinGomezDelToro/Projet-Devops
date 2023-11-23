5eeb2bb8df524bf3b64c92949e33288b41225a0b
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-11-23 09:41:19 +0100

    login updated

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import axios from 'axios';
 import { useRouter } from 'next/router';
 import { Box, Button, Stack, Center, FormControl, FormLabel, FormErrorMessage, Input, Text, Heading } from '@chakra-ui/react';
 import { Field, Form, Formik, FormikHelpers, FieldProps } from 'formik';
@@ -10,51 +9,40 @@ interface LoginCredentials {
     password: string;
 }
 
-// Función para iniciar sesión
-export const loginUser = async (credentials: LoginCredentials): Promise<string | false> => {
-    try {
-        const response = await axios.post("/api/auth/login", credentials);
-        console.log("Respuesta de loginUser", response.data); // Depuración: Imprime la respuesta
-        if (response.data.message === "Connexion réussie !") {
-            return response.data.token;
-        } else {
-            console.log(response.data);
-            return false;
-        }
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error("Erreur lors de la connexion :", error.message);
-        } else {
-            console.error("Erreur lors de la connexion :", error);
-        }
-        return false;
-    }
-};
-
-// Esquema de validación
-const validationSchema = Yup.object({
-    email: Yup.string().email("Email non valide").required("Champ requis"),
-    password: Yup.string().required("Champ requis"),
-});
-
 function Login() {
     const router = useRouter();
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
     const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-    const handleSubmit = async (values: LoginCredentials, actions: FormikHelpers<LoginCredentials>) => {
-        console.log("handleSubmit ejecutado", values); // Depuración: Confirma que handleSubmit se llama
-        const token = await loginUser(values);
-        actions.setSubmitting(false);
+    const validationSchema = Yup.object({
+        email: Yup.string().email("Email non valide").required("Champ requis"),
+        password: Yup.string().required("Champ requis"),
+    });
 
-        if (token) {
-            console.log("Intentando redirigir a /dashboard"); // Depuración: Confirma intento de redirección
-            setSuccessMessage("Connexion réussie !");
-            setErrorMessage(null);
-            router.push('/dashboard').catch(err => console.error("Error en redirección", err)); // Captura errores de redirección
-        } else {
-            setErrorMessage("Échec de la connexion. Veuillez réessayer.");
-            setSuccessMessage(null);
+    const handleSubmit = async (values: LoginCredentials, actions: FormikHelpers<LoginCredentials>) => {
+        try {
+            const response = await fetch("/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                setErrorMessage(errorData.message);
+            } else {
+                await router.push('/dashboard');
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error("Erreur lors de la connexion :", error.message);
+                setSuccessMessage("Connexion réussie !");
+                setErrorMessage("Échec de la connexion. Veuillez réessayer.");
+            }
+        } finally {
+            actions.setSubmitting(false);
         }
     };
 
